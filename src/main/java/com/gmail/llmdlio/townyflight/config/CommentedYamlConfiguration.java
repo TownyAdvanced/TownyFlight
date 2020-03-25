c42c9effba982026dd3d412b8f825408e54e82e5
UberPilot <uberpilot99@gmail.com>
2020-03-25 14:05:02 -0600

    Remove unneeded import.

--- src/main/java/com/gmail/llmdlio/townyflight/config/CommentedYamlConfiguration.java
+++ src/main/java/com/gmail/llmdlio/townyflight/config/CommentedYamlConfiguration.java
@@ -2,7 +2,6 @@ package com.gmail.llmdlio.townyflight.config;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
