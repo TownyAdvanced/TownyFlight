0a63566d81ed2d11bff692b08b971b5df9b56089
Llm Dl <LlmDlio@gmail.com>
2022-01-04 10:29:32 -0600

    1.8.2:
    Adapt to spigot config changes for MC 1.18.1.

---
+++ src/main/java/com/gmail/llmdlio/townyflight/config/ConfigNodes.java
@@ -0,0 +1,180 @@
+package com.gmail.llmdlio.townyflight.config;
+
+public enum ConfigNodes {
+	VERSION("Version", ""),
+	PLUGIN_PREFIX("pluginPrefix",
+			"&8[&3TownyFlight&8] ",
+			""),
+	LANG("language", "", "",
+			"####################",
+			"# Language Strings #",
+			"####################"),
+	LANG_FLIGHTON(
+			"language.flightOnMsg",
+			"Flight Activated. ",
+			"",
+			"# Message shown when flight activated."),
+	LANG_FLIGHTOFF(
+			"language.flightOffMsg",
+			"Flight De-Activated. ",
+			"",
+			"# Message shown when flight de-activated."),
+	LANG_NOTOWN(
+			"language.noTownMsg",
+			"Flight cannot be activated, you don't belong to a town. ",
+			"",
+			"# Message shown when player lacks a town. "),
+	LANG_NOTINTOWN(
+			"language.notInTownMsg",
+			"Flight cannot be activated, return to your town and try again. ",
+			"",
+			"# Message shown when flight cannot be turned on."),
+	LANG_FLIGHTDEACTIVATEDMSG(
+			"language.flightDeactivatedMsg",
+			"Left town boundaries. ",
+			"",
+			"# Message shown when a player has flight taken away."),
+	LANG_FLIGHTDEACTIVATEDPVPMSG(
+			"language.flightDeactivatedPVPMsg",
+			"Entering PVP combat. ",
+			"",
+			"# Message shown when a player has flight taken away because of PVP."),
+	LANG_FLIGHTDEACTIVATEDCONSOLEMSG(
+			"language.flightDeactivatedConsoleMsg",
+			"Flight priviledges removed. ",
+			"",
+			"# Message shown when a player has flight taken away by console."),
+	LANG_NOPERMISSION(
+			"language.noPermission",
+			"You do not have permission for this command%s. ",
+			"",
+			"# Message shown when a player lacks a permission node."),
+	LANG_MISSINGNODE(
+			"language.missingNode",
+			", missing %s",
+			"",
+			"# Message attached to noPermission when options.show_Permission_After_No_Permission_Message is true"),
+	LANG_NOTDURINGWAR(
+			"language.notDuringWar", 
+			"You cannot use flight while Towny war is active. ",
+			"",
+			"# Message shown when war is active and flight is disallowed."),
+	LANG_RETURNTOALLOWEDAREA(
+			"language.returnToAllowedArea",
+			"You have %s seconds to return to an allowed flight area. ",
+			"",
+			"# Message telling a player to return to an allowed flight area."),
+	LANG_NOTOWNFOUND(
+			"language.noTownFound",
+			"TownyFlight cannot find a town by the name %s. ",
+			"",
+			"# Message when a town cannot be found by the name."),
+	LANG_TOWNWIDEFLIGHT(
+			"language.townWideFlight",
+			"Free flight has been %s in %s. ",
+			"",
+			"# Message when a town has free flight enabled or disabled."),
+	LANG_DISABLED(
+			"language.disabled", 
+			"disabled",
+			"",
+			"# The word disabled."),
+	LANG_ENABLED(
+			"language.enabled",
+			"enabled",
+			"",
+			"# The world enabled."),
+	LANG_STATUSSCREENCOMP(
+			"language.statusScreenComponent", 
+			"Free Flight",
+			"",
+			"# The component shown on towns' status screens when they have free flight enabled."),
+	LANG_STATUSCOMPHOVER(
+			"language.statusScreenComponentHover",
+			"Flight enabled for everyone within this town's borders.",
+			"",
+			"# The hover text shown on the free flight status screen component."),
+	
+	OPTIONS("options", "", "",
+			"#################", 
+			"#    Options    #", 
+			"#################"),
+	OPTIONS_AUTO_ENABLE_FLIGHT(
+			"options.auto_Enable_Flight",
+			"false",
+			"",
+			"# If set to true, players entering their town will have flight auto-enabled.",
+			"# When set to true, the plugin will use slightly more resources due to the EnterTown listener."),
+	OPTIONS_AUTO_ENABLE_SILENT(
+			"options.auto_Enable_Silent", 
+			"false",
+			"",
+			"# If set to true, players entering their town will have flight auto-enabled without being notified in chat."),
+	OPTIONS_DISABLE_DURING_WARTIME(
+			"options.disable_During_Wartime", 
+			"true",
+			"",
+			"# If set to false, players can still fly in their town while war is active."),
+	OPTIONS_DISABLE_COMBAT_PREVENT(
+			"options.disable_Combat_Prevention", 
+			"false",
+			"",
+			"# If set to false, TownyFlight will not prevent combat of flying people."),
+	OPTIONS_SHOW_PERMISSION(
+			"options.show_Permission_After_No_Permission_Message", 
+			"true",
+			"",
+			"# If set to false, the language.noPermission message will not display the permission node."),
+	OPTIONS_DISABLE_TIME(
+			"options.flight_Disable_Timer",
+			"3",
+			"",
+			"# Number of seconds after leaving an allowed flight area before flight is taken away.", "# Set to 0 to take flight away immediately.");
+
+	private final String Root;
+	private final String Default;
+	private String[] comments;
+
+	ConfigNodes(String root, String def, String... comments) {
+
+		this.Root = root;
+		this.Default = def;
+		this.comments = comments;
+	}
+
+	/**
+	 * Retrieves the root for a config option
+	 *
+	 * @return The root for a config option
+	 */
+	public String getRoot() {
+
+		return Root;
+	}
+
+	/**
+	 * Retrieves the default value for a config path
+	 *
+	 * @return The default value for a config path
+	 */
+	public String getDefault() {
+
+		return Default;
+	}
+
+	/**
+	 * Retrieves the comment for a config path
+	 *
+	 * @return The comments for a config path
+	 */
+	public String[] getComments() {
+
+		if (comments != null) {
+			return comments;
+		}
+
+		String[] comments = new String[1];
+		comments[0] = "";
+		return comments;
+	}
+}
--- src/main/java/com/gmail/llmdlio/townyflight/config/TownyFlightConfig.java
+++ src/main/java/com/gmail/llmdlio/townyflight/config/TownyFlightConfig.java
@@ -2,13 +2,15 @@ package com.gmail.llmdlio.townyflight.config;
 
 import java.io.File;
 import java.io.IOException;
-import org.bukkit.configuration.InvalidConfigurationException;
+import java.nio.file.Path;
 
 import com.gmail.llmdlio.townyflight.TownyFlight;
+import com.palmergames.bukkit.config.CommentedConfiguration;
 
 public class TownyFlightConfig {
 	private TownyFlight plugin;
-	private CommentedYamlConfiguration config;
+	private static CommentedConfiguration config;
+	private static CommentedConfiguration newConfig;
 
 	public TownyFlightConfig(TownyFlight plugin) {
 		this.plugin = plugin;
@@ -30,104 +32,51 @@ public class TownyFlightConfig {
 			}
 		}
 
-		config = new CommentedYamlConfiguration();
+		config = new CommentedConfiguration(f.toPath());
 
-		try {
-			config.load(f);
-		} catch (IOException | InvalidConfigurationException e) {
-			plugin.getLogger().severe("Config.yml Error: " + e.getMessage());
+		if (!config.load())
 			return false;
-		}
 
-		addComment("Version", "# TownyFlight by LlmDl.");
-		addDefault("Version", plugin.getDescription().getVersion());
-		addComment("pluginPrefix", "# Prefix to messages seen in game.");
-		addDefault("pluginPrefix", "&8[&3TownyFlight&8] ");
+		setDefaults(plugin.getDescription().getVersion(), f.toPath());
 
-		addComment("language", "", "",
-				"####################",
-				"# Language Strings #",
-				"####################", "");
-		addComment("language.flightOnMsg", "# Message shown when flight activated.");
-		addDefault("language.flightOnMsg", "Flight Activated. ");
-		addComment("language.flightOffMsg", "# Message shown when flight de-activated.");
-		addDefault("language.flightOffMsg", "Flight De-activated. ");
-		addComment("language.noTownMsg", "# Message shown when player lacks a town. ");
-		addDefault("language.noTownMsg", "Flight cannot be activated, you don't belong to a town. ");
-		addComment("language.notInTownMsg", "# Message shown when flight cannot be turned on.");
-		addDefault("language.notInTownMsg", "Flight cannot be activated, return to your town and try again. ");
-		addComment("language.flightDeactivatedMsg", "# Message shown when a player has flight taken away.");
-		addDefault("language.flightDeactivatedMsg", "Left town boundaries. ");
-		addComment("language.flightDeactivatedPVPMsg","# Message shown when a player has flight taken away because of PVP.");
-		addDefault("language.flightDeactivatedPVPMsg", "Entering PVP combat. ");
-		addComment("language.flightDeactivatedConsoleMsg", "# Message shown when a player has flight taken away by console.");
-		addDefault("language.flightDeactivatedConsoleMsg", "Flight priviledges removed. ");
-		addComment("language.noPermission", "# Message shown when a player lacks a permission node.");
-		addDefault("language.noPermission", "You do not have permission for this command%s. ");
-		addComment("language.missingNode", "# Message attached to noPermission when options.show_Permission_After_No_Permission_Message is true");
-		addDefault("language.missingNode", ", missing %s");
-		addComment("language.notDuringWar", "# Message shown when war is active and flight is disallowed.");
-		addDefault("language.notDuringWar", "You cannot use flight while Towny war is active. ");
-		addComment("language.returnToAllowedArea", "# Message telling a player to return to an allowed flight area.");
-		addDefault("language.returnToAllowedArea", "You have %s seconds to return to an allowed flight area. ");
-		addComment("language.noTownFound", "# Message when a town cannot be found by the name.");
-		addDefault("language.noTownFound", "TownyFlight cannot find a town by the name %s. ");
-		addComment("language.townWideFlight", "# Message when a town has free flight enabled or disabled.");
-		addDefault("language.townWideFlight", "Free flight has been %s in %s. ");
-		addComment("language.disabled", "# The word disabled.");
-		addDefault("language.disabled", "disabled");
-		addComment("language.enabled", "# The world enabled.");
-		addDefault("language.enabled", "enabled");
-		addComment("language.statusScreenComponent", "# The component shown on towns' status screens when they have free flight enabled.");
-		addDefault("language.statusScreenComponent", "Free Flight");
-		addComment("language.statusScreenComponentHover", "# The hover text shown on the free flight status screen component.");
-		addDefault("language.statusScreenComponentHover", "Flight enabled for everyone within this town's borders.");
+		config.save();
 
-		addComment("options", "", "", "", 
-				"#################", 
-				"#    Options    #", 
-				"#################", "");
-		addComment("options.auto_Enable_Flight",
-				"# If set to true, players entering their town will have flight auto-enabled.",
-				"# When set to true, the plugin will use slightly more resources due to the EnterTown listener.");
-		addDefault("options.auto_Enable_Flight", "false");
-		addComment("options.auto_Enable_Silent", "# If set to true, players entering their town will have flight auto-enabled without being notified in chat.");
-		addDefault("options.auto_Enable_Silent", "false");
-		addComment("options.disable_During_Wartime", "# If set to false, players can still fly in their town while war is active.");
-		addDefault("options.disable_During_Wartime", "true");
-		addComment("options.disable_Combat_Prevention", "# If set to false, TownyFlight will not prevent combat of flying people.");
-		addDefault("options.disable_Combat_Prevention", "false");
-		addComment("options.show_Permission_After_No_Permission_Message", "# If set to false, the language.noPermission message will not display the permission node.");
-		addDefault("options.show_Permission_After_No_Permission_Message", "true");
-		addComment("options.flight_Disable_Timer", "# Number of seconds after leaving an allowed flight area before flight is taken away.", "# Set to 0 to take flight away immediately.");
-		addDefault("options.flight_Disable_Timer", "3");
-
-		// Write back config
-		try {
-			config.save(f);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
 		return true;
 	}
 
-	public CommentedYamlConfiguration getConfig() {
+	/**
+	 * Builds a new config reading old config data.
+	 */
+	private static void setDefaults(String version, Path configPath) {
+		newConfig = new CommentedConfiguration(configPath);
+		newConfig.load();
+
+		for (ConfigNodes node : ConfigNodes.values()) {
+			String root = node.getRoot();
+			if (node.getComments().length > 0)
+				addComment(root, node.getComments());
+
+			if (root.equals(ConfigNodes.VERSION.getRoot()))
+				setNewProperty(root, version);
+			else
+				setNewProperty(root, (config.get(root) != null) ? config.get(root) : node.getDefault());
+		}
+
+		config = newConfig;
+		newConfig = null;
+	}
+
+	public CommentedConfiguration getConfig() {
 		return config;
 	}
 
-	private boolean hasPath(String path) {
-		return config.isSet(path);
+	private static void addComment(String root, String... comments) {
+		newConfig.addComment(root, comments);
 	}
 
-	private void addComment(String path, String... comment) {
-		config.addComment(path, comment);
-	}
-
-	private void addDefault(String path, Object defaultValue) {
-		if (path.equals("Version"))
-			config.set(path, plugin.getDescription().getVersion());
-		if (!hasPath(path))
-			config.set(path, defaultValue);
+	private static void setNewProperty(String root, Object value) {
+		if (value == null)
+			value = "";
+		newConfig.set(root, value.toString());
 	}
 }
