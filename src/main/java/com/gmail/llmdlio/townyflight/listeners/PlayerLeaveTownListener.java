28bc5db1aa5bbe65c3b4fcc78cb6f95295208b2e
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-05-09 21:42:45 +0200

    Folia Support

--- src/main/java/com/gmail/llmdlio/townyflight/listeners/PlayerLeaveTownListener.java
+++ src/main/java/com/gmail/llmdlio/townyflight/listeners/PlayerLeaveTownListener.java
@@ -1,5 +1,6 @@
 package com.gmail.llmdlio.townyflight.listeners;
 
+import com.gmail.llmdlio.townyflight.TownyFlight;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,10 +9,14 @@ import org.bukkit.event.Listener;
 import com.gmail.llmdlio.townyflight.TownyFlightAPI;
 import com.gmail.llmdlio.townyflight.config.Settings;
 import com.gmail.llmdlio.townyflight.util.Message;
-import com.gmail.llmdlio.townyflight.util.Scheduler;
 import com.palmergames.bukkit.towny.event.player.PlayerExitsFromTownBorderEvent;
 
-public class PlayerLeaveTownListener implements Listener{	
+public class PlayerLeaveTownListener implements Listener {
+	private final TownyFlight plugin;
+
+	public PlayerLeaveTownListener(TownyFlight plugin) {
+		this.plugin = plugin;
+	}
 
 	/*
 	 * Listener for a player who leaves town. Runs one tick after the
@@ -23,7 +28,7 @@ public class PlayerLeaveTownListener implements Listener{
 		if (!player.getAllowFlight() || player.hasPermission("townyflight.bypass"))
 			return;
 
-		Scheduler.run(() -> executeLeaveTown(player), 1);
+		plugin.getScheduler().runLater(player, () -> executeLeaveTown(player), 1);
 	}
 
 	/*
@@ -36,7 +41,7 @@ public class PlayerLeaveTownListener implements Listener{
 				TownyFlightAPI.getInstance().removeFlight(player, false, true, "");
 			} else {
 				Message.of(String.format(Message.getLangString("returnToAllowedArea"), Settings.flightDisableTimer)).serious().to(player);
-				Scheduler.run(() -> TownyFlightAPI.getInstance().testForFlight(player, true), Settings.flightDisableTimer * 20);
+				plugin.getScheduler().runLater(player, () -> TownyFlightAPI.getInstance().testForFlight(player, true), Settings.flightDisableTimer * 20);
 			}
 		}
 	}
